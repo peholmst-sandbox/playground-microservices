6c9b27107d6370e7f51b40dccbadbc67c2c02532
Petter Holmström <petter@vaadin.com>
2019-01-22 11:46:15 +0200

    Add ResourceDescriptor and ResourceRegistration

---
+++ service-directory/service-directory-api/src/main/java/net/pkhapps/playground/microservices/directory/api/ResourceDescriptor.java
@@ -0,0 +1,95 @@
+package net.pkhapps.playground.microservices.directory.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.Nullable;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Base class for resource descriptors.
+ *
+ * @param <ID> the resource ID type.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public abstract class ResourceDescriptor<ID> implements Serializable {
+
+    @JsonProperty
+    private final ID id;
+    @JsonProperty
+    private final String name;
+    @JsonProperty
+    private final String description;
+    @JsonProperty
+    private final URI iconUri;
+
+    /**
+     * Creates a new resource descriptor.
+     *
+     * @param id          the ID of the resource.
+     * @param name        the human readable name of the resource.
+     * @param description an optional human readable description of the resource.
+     * @param iconUri     an optional URI of an icon for the resource.
+     */
+    @JsonCreator
+    public ResourceDescriptor(@JsonProperty(value = "id", required = true) ID id,
+                              @JsonProperty(value = "name", required = true) String name,
+                              @JsonProperty(value = "description") @Nullable String description,
+                              @JsonProperty(value = "iconUri") @Nullable URI iconUri) {
+        this.id = Objects.requireNonNull(id, "id must not be null");
+        this.name = Objects.requireNonNull(name, "name must not be null");
+        this.description = description;
+        this.iconUri = iconUri;
+    }
+
+    /**
+     * Returns the ID of the resource.
+     */
+    public ID getId() {
+        return id;
+    }
+
+    /**
+     * Returns the human readable name of the resource.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Returns the optional human readable description of the resource.
+     */
+    @JsonIgnore
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(description);
+    }
+
+    /**
+     * Returns the optional icon URI of the resource.
+     */
+    @JsonIgnore
+    public Optional<URI> getIconUri() {
+        return Optional.ofNullable(iconUri);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ResourceDescriptor<?> that = (ResourceDescriptor<?>) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(description, that.description) &&
+                Objects.equals(iconUri, that.iconUri);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, description, iconUri);
+    }
+}
---
+++ service-directory/service-directory-api/src/main/java/net/pkhapps/playground/microservices/directory/api/ResourceRegistration.java
@@ -0,0 +1,99 @@
+package net.pkhapps.playground.microservices.directory.api;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.Nullable;
+
+import java.net.URI;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Objects;
+
+/**
+ * Base class for resource registration objects. The registration contains a public key that must be used by the service
+ * directory when verifying incoming resource instance registrations.
+ *
+ * @param <ID> the resource ID type.
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public abstract class ResourceRegistration<ID> extends ResourceDescriptor<ID> {
+
+    @JsonProperty
+    private final String algorithm;
+    @JsonProperty
+    private final String publicKey;
+    @JsonIgnore
+    private transient PublicKey cachedPublicKey;
+
+    /**
+     * Creates a new resource registration.
+     *
+     * @param id          the ID of the resource.
+     * @param name        the human readable name of the resource.
+     * @param description an optional human readable description of the resource.
+     * @param iconUri     an optional URI of an icon for the resource.
+     * @param publicKey   the public key of the service. The public key must support {@link PublicKey#getEncoded() encoding}.
+     */
+    public ResourceRegistration(ID id, String name, @Nullable String description, @Nullable URI iconUri,
+                                PublicKey publicKey) {
+        super(id, name, description, iconUri);
+        Objects.requireNonNull(publicKey, "publicKey must not be null");
+        this.algorithm = publicKey.getAlgorithm();
+        this.publicKey = Base64.getEncoder().encodeToString(Objects.requireNonNull(publicKey.getEncoded(),
+                "publicKey cannot be encoded"));
+        this.cachedPublicKey = publicKey;
+    }
+
+    /**
+     * Constructor used by Jackson and unit tests only. Clients should not use directly.
+     */
+    @JsonCreator
+    protected ResourceRegistration(@JsonProperty(value = "id", required = true) ID id,
+                                   @JsonProperty(value = "name", required = true) String name,
+                                   @JsonProperty(value = "description") @Nullable String description,
+                                   @JsonProperty(value = "iconUri") @Nullable URI iconUri,
+                                   @JsonProperty(value = "algorithm", required = true) String algorithm,
+                                   @JsonProperty(value = "publicKey", required = true) String publicKey) {
+        super(id, name, description, iconUri);
+        this.algorithm = algorithm;
+        this.publicKey = publicKey;
+    }
+
+    /**
+     * Returns the public key of the resource. This key is used by the service directory to verify the signatures of all
+     * resource instance registrations before they are accepted.
+     */
+    @JsonIgnore
+    public PublicKey getPublicKey() {
+        try {
+            if (this.cachedPublicKey == null) {
+                this.cachedPublicKey = KeyFactory.getInstance(algorithm)
+                        .generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey)));
+            }
+            return this.cachedPublicKey;
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+            throw new IllegalStateException("Could not generate public key", ex);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        ResourceRegistration<?> that = (ResourceRegistration<?>) o;
+        return Objects.equals(algorithm, that.algorithm) &&
+                Objects.equals(publicKey, that.publicKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), algorithm, publicKey);
+    }
+}
